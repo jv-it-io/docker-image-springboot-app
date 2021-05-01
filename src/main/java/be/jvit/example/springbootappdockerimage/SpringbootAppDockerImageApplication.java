0e5ec99d3e51c115576f8904c7eb0017c9fccebe
Jonathan <vandersmissen.jonathan@gmail.com>
2021-05-01 19:50:46 +0200

    init

---
+++ src/main/java/be/jvit/example/springbootappdockerimage/SpringbootAppDockerImageApplication.java
@@ -0,0 +1,13 @@
+package be.jvit.example.springbootappdockerimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppDockerImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppDockerImageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/jvit/example/springbootappdockerimage/SpringbootAppDockerImageApplicationTests.java
@@ -0,0 +1,13 @@
+package be.jvit.example.springbootappdockerimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppDockerImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
